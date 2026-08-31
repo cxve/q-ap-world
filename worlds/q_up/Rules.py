c5e2c88486b93665d03696830e2de290328c245c
cxve <20140539+cxve@users.noreply.github.com>
2026-09-01 01:54:22 +0200

    tweak: make shop logic a bit slower

--- worlds/q_up/Rules.py
+++ worlds/q_up/Rules.py
@@ -62,7 +62,7 @@ class QUPrules:
         cost = shop_costs[location]
         num_skills = self.world.options.itemPoolTotalSkillNum.value
         req_rank = data["rank"] if "rank" in data else 0
-        if req_rank < 1 and data["cost"] > 300: req_rank = 30
+        if req_rank < 30 and data["cost"] > 300: req_rank = 30
         efficiency_crystals = self.world.options.itemPoolEfficiencyCrystals.value
         efficiency_corruption_shards = self.world.options.itemPoolEfficiencyCorruptionShards.value
         min_crystals = self.world.options.itemPoolCrystalNum.value
@@ -74,7 +74,7 @@ class QUPrules:
             count_crystals = state.count("Crystals", self.player) * efficiency_crystals * (count_recycling + 1)
             crystals = sum(crystal_rewards[0:count_crystals])
             crystals += max(0, count_crystals - len(crystal_rewards)) * 100
-            crystals += sum(mail_crystal_rewards[0:count_crystals + 1])
+            crystals += sum(mail_crystal_rewards[0:floor(count_crystals / 2) + 1])
             has_crystals = crystals >= cost or (count_crystals >= min_crystals and count_recycling == 2)
             return has_crystals
 
